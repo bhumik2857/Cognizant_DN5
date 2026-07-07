a429b4c078663e89092ab4194874f9887f73eaf8
Bhumika Gupta <bhumika1511gupt@gmail.com>
2026-07-07 13:39:09 +0530

    Implement Singleton Design Pattern

--- week1/Week1-DesignPatterns/SingletonPatternExample.java
+++ week1/Week1-DesignPatterns/SingletonPatternExample.java
@@ -1,19 +1,23 @@
 class Logger {
 
-    private static Logger obj;
+    // Single instance
+    private static Logger instance;
 
+    // Private constructor
     private Logger() {
     }
 
+    // Method to get the single instance
     public static Logger getInstance() {
-        if (obj == null) {
-            obj = new Logger();
+        if (instance == null) {
+            instance = new Logger();
         }
-        return obj;
+        return instance;
     }
 
-    public void display(String msg) {
-        System.out.println(msg);
+    // Log method
+    public void display(String message) {
+        System.out.println("Log: " + message);
     }
 }
 
@@ -21,16 +25,16 @@ public class SingletonPatternExample {
 
     public static void main(String[] args) {
 
-        Logger l1 = Logger.getInstance();
-        Logger l2 = Logger.getInstance();
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
 
-        l1.display("First Log Message");
-        l2.display("Second Log Message");
+        logger1.display("Application Started");
+        logger2.display("User Logged In");
 
-        if (l1 == l2) {
-            System.out.println("Only one object created");
+        if (logger1 == logger2) {
+            System.out.println("Singleton Pattern Verified: Only one Logger object exists.");
         } else {
-            System.out.println("Different objects created");
+            System.out.println("Multiple Logger objects created.");
         }
     }
 }
