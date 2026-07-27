42e488247b7920af2326b361bcf404ba60fd2deb
Bhumika Gupta <bhumika1511gupt@gmail.com>
2026-07-27 22:49:51 +0530

    Complete Angular Hands-on 1 - Student Course Portal setup

---
+++ week6/Angular/Handson1-StudentCoursePortal/src/app/pages/course-list/course-list.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  template: `<h2>Course List Page</h2>`,
+})
+export class CourseList {}
---
+++ week6/Angular/Handson1-StudentCoursePortal/src/app/pages/student-profile/student-profile.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-profile',
+  standalone: true,
+  template: `<h2>Student Profile Page</h2>`,
+})
+export class StudentProfile {}
