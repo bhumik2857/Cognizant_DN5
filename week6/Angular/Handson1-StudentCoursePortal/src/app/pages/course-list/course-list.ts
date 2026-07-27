f224b2bd112a075e1633fee8a71e664d37a41338
Bhumika Gupta <bhumika1511gupta@gmail.com>
2026-07-27 23:56:52 +0530

    Removed Angular hands-on folder

--- week6/Angular/Handson1-StudentCoursePortal/src/app/pages/course-list/course-list.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-course-list',
-  standalone: true,
-  template: `<h2>Course List Page</h2>`,
-})
-export class CourseList {}
